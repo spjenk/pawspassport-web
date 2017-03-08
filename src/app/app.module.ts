ece5a4d89c411a178f5d5d9d510997e6f7f818c9
Stephen Jenkinson <stephen.jenkinson@tattsgroup.com>
2017-03-08 14:11:27 +1000

    Remove Sign-up dialog.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
-
-import { SignUpModule } from './sign-up/sign-up.module'
-import { AvailabilityModule } from "./availability/availability.module";
 import {routing} from "./app.routes";
-
-
 
 @NgModule({
   declarations: [
@@ -17,8 +11,7 @@ import {routing} from "./app.routes";
   imports: [
     BrowserModule,
     HttpModule,
-    routing,
-    SignUpModule
+    routing
   ],
   providers: [ ],
   bootstrap: [AppComponent]
